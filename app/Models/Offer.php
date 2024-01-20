e2a64a99194963aa0818da90bcfd76d5be2238ce
monir0153 <mohammadmonir0153@gmail.com>
2024-01-20 23:52:37 +0600

    constant class create, category , offer, location database factory seeder created

---
+++ app/Models/Offer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Offer extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'price', 'description', 'author_id', 'status'];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+    public function locations(): BelongsToMany
+    {
+        return $this->belongsToMany(Location::class);
+    }
+}
---
+++ database/seeders/OfferSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Location;
+use App\Models\Offer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OfferSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $offfers = Offer::factory()->count(5)->create();
+
+        foreach ($offfers as $offer) {
+            $categories = Category::inRandomOrder()->limit(5)->get();
+            $offer->categories()->sync($categories->pluck('id'));
+        }
+        foreach ($offfers as $offer) {
+            $locations = Location::inRandomOrder()->limit(5)->get();
+            $offer->locations()->sync($locations->pluck('id'));
+        }
+    }
+}
